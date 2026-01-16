53e02d26f2df1385ac77e791193141a4977b8b2a
StarBoy200614 <manaspandya2006@gmail.com>
2026-01-16 04:52:04 +0000

    Learnt datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,7 +4,13 @@ let name = "Manas"
 let age = 19
 let pass = "a1b2c3"
 
+//undefined => when value is not given 
+//null => when you want to give no value
+//symbol => for unique
 console.log(name)
 console.log(age)
 console.log(pass)
 
+
+//object
+ 
